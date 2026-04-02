8e7053da7ab9a3f7c9de8b16a8ccfc2524ce8e3a
yjs0601 <soo00@tukorea.ac.kr>
2026-04-02 14:04:23 +0900

    Title: 물의 상태, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/물의 상태/state-of-water.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin>>n;
+
+    if(n <0)
+    {
+        cout<<"ice"<<endl;
+    }
+
+    else if(n>=100)
+    {
+        cout<<"vapor"<<endl;
+    }
+    else
+    {
+        cout<<"water";
+    }
+        
+
+    return 0;
+}
