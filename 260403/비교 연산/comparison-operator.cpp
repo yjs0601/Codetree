3390b311325ecbaa22e9a7d086aae7950cff6358
yjs0601 <soo00@tukorea.ac.kr>
2026-04-03 15:08:06 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/비교 연산/comparison-operator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+
+    cin>>a>>b;
+
+    if(a>=b)
+    {
+        cout<<1<<endl;
+    }
+    else
+    cout<<0<<endl;
+   if(a>b)
+    {
+        cout<<1<<endl;
+    }
+    else
+    cout<<0<<endl;
+    if(a<=b)
+    {
+        cout<<1<<endl;
+    }
+    else
+    cout<<0<<endl;
+    if(a<b)
+    {
+        cout<<1<<endl;
+    }
+    else
+    cout<<0<<endl;
+    if(a==b)
+    {
+        cout<<1<<endl;
+    }
+    else
+    cout<<0<<endl;
+    if(a!=b)
+    {
+        cout<<1<<endl;
+    }
+    else
+    cout<<0<<endl;
+    return 0;
+}
