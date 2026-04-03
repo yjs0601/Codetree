3e20b04292d5719b54998323cfe76d75ab818062
yjs0601 <soo00@tukorea.ac.kr>
2026-04-03 14:48:32 +0900

    Title: 두 수의 짝홀 여부, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/두 수의 짝홀 여부/parity-of-two-numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+
+    cin>> a>> b;
+    
+    if(a%2==0)
+    {
+        cout<<"even"<<endl;
+    }
+    else
+    {
+        cout<<"odd"<<endl;
+    }
+
+    if(b%2==0)
+    {
+        cout<<"even"<<endl;
+    }
+    else
+    {
+        cout<<"odd"<<endl;
+    }
+    return 0;
+}
