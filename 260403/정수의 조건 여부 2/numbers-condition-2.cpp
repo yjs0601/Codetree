39e6bdacbda2f0bba7d87c69e5aaa6de42e1531c
yjs0601 <soo00@tukorea.ac.kr>
2026-04-03 14:43:46 +0900

    Title: 정수의 조건 여부 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin>> a;
+
+    if(a==5)
+    {
+        cout<<"A"<<endl;
+    }
+    if(a%2==0)
+    {
+        cout<<"B"<<endl;
+    }
+    return 0;
+}
