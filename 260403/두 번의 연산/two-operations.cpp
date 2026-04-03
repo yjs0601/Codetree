b565a49a28d92e6297401f92cce3efbcf5709aa4
yjs0601 <soo00@tukorea.ac.kr>
2026-04-03 14:46:14 +0900

    Title: 두 번의 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/두 번의 연산/two-operations.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a%2==1)
+    {
+        a=a+3;
+    }
+    if(a%3==0)
+    {
+        a=a/3;
+    }
+    cout<<a<<endl;
+    return 0;
+}
