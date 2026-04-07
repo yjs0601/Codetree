27a9b3ac9fbf388e1d28c602d866aa86bb4d977c
yjs0601 <soo00@tukorea.ac.kr>
2026-04-07 11:14:59 +0900

    Title: B부터 A까지 감소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A,B;
+
+    cin>>A>>B;
+
+    for(B ; B>=A;B--)
+    {
+        cout<<B<<" ";
+    }
+    return 0;
+}
