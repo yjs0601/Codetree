e5b8d41a791b3292dd656e5ac5cb2939b9a71007
yjs0601 <soo00@tukorea.ac.kr>
2026-04-07 11:19:26 +0900

    Title: 홀수만 출력 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int B,A;
+    int odd;
+    cin>>B>>A;
+
+    for(B;B>=A;B-=2)
+    {
+        cout<<B<<" ";
+    }
+    return 0;
+}
