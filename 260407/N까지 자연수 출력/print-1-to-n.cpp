d7a440a139c4f54ad15513ede4ddf09913c1974d
yjs0601 <soo00@tukorea.ac.kr>
2026-04-07 11:22:54 +0900

    Title: N까지 자연수 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    int a = 0;
+    cin>>N;
+
+    while(a!=N)
+    {
+        a++;
+        cout<<a<<" ";
+        
+    }
+    return 0;
+}
