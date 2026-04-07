719544320fd6c01ecfb632e2f5e63d0efac1f9cc
yjs0601 <soo00@tukorea.ac.kr>
2026-04-07 11:28:12 +0900

    Title: 별 N개 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+
+    cin>> N;
+
+    while(N!=0)
+    {
+        cout<<"*"<<endl;
+        N--;
+    }
+    return 0;
+}
