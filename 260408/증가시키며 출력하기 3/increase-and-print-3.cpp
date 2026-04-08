48bc4ed883f457bf99db373571377d6a02e1fb0b
yjs0601 <soo00@tukorea.ac.kr>
2026-04-08 20:32:56 +0900

    Title: 증가시키며 출력하기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 10;
+
+    while(a<=26)
+    {
+        cout<<a<< " ";
+        a+=2;
+    }
+    return 0;
+}
