b5467b654d554eb43c64289625ff08059c490c6a
yjs0601 <soo00@tukorea.ac.kr>
2026-04-08 20:38:42 +0900

    Title: 짝수만 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A,B;
+    cin>>A>>B;
+
+    while(A<=B)
+    {
+        if(A%2==0)
+        {
+            cout<<A<<" ";
+        }
+        A++;
+    }
+    return 0;
+}
