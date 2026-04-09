9ec73554edfc6ff5f3e59a7fc840b82da20c99ce
yjs0601 <soo00@tukorea.ac.kr>
2026-04-09 12:51:16 +0900

    Title: 감소시키며 출력하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260409/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 26;
+
+    while(a!=9)
+    {
+        cout<<a<< " ";
+        a--;
+    }
+    return 0;
+}
