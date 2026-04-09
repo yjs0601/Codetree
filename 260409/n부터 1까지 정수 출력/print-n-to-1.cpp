1487364359317600b494cbeffc0ffa781ad6f7f0
yjs0601 <soo00@tukorea.ac.kr>
2026-04-09 12:56:24 +0900

    Title: n부터 1까지 정수 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260409/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+
+    cin>>n;
+
+    while(n>=1)
+    {
+        cout<< n << " ";
+        n--;
+    }
+    return 0;
+}
