d6f0d3278c08ac6c2ee6e1956eb4db039ad71166
yjs0601 <soo00@tukorea.ac.kr>
2026-04-10 21:15:19 +0900

    Title: 문자 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260410/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char a;
+    cin>>a;
+
+    for (int i=1;i<9;i++)
+    {
+        cout<<a;
+    }
+
+    return 0;
+}
