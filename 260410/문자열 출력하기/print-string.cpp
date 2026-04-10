72cdd2e9d3d9734e8187278edc1244dab94bd9ef
yjs0601 <soo00@tukorea.ac.kr>
2026-04-10 21:16:57 +0900

    Title: 문자열 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260410/문자열 출력하기/print-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin>>N;
+
+    for(int i=1;i<=N;i++)
+    {
+        cout<<"LeebrosCode"<<endl;
+    }
+    return 0;
+}
