02480679dce12417e997c6ad3163c9785dce4a14
yjs0601 <soo00@tukorea.ac.kr>
2026-04-11 23:30:08 +0900

    Title: N번 더하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260411/N번 더하기/add-n-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A,N;
+    cin>>A>>N;
+
+    for(int i=1;i<=N;i++)
+    {
+        A=A+N;
+        cout<<A<<endl;
+    }
+    return 0;
+}
