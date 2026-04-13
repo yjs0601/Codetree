712c070af3754039b925371088b77ce17f918598
yjs0601 <soo00@tukorea.ac.kr>
2026-04-13 13:41:55 +0900

    Title: 높은 수에서 낮은 수까지, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260413/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A,B;
+    cin>>A>>B;
+
+    if(A>B)
+    {
+        for(A;A>=B;A--)
+        {
+            cout<<A<<" ";
+        }
+    }
+    else if (A<B)
+    {
+        for(B;B>=A;B--)
+        {
+            cout<<B<<" ";
+        }
+    }
+    return 0;
+}
