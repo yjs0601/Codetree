91c1af82380029554e6ba5decef97238d6a943d3
yjs0601 <soo00@tukorea.ac.kr>
2026-04-17 11:12:31 +0900

    Title: 일 수 구하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260417/일 수 구하기/number-of-days-in-month.cpp
+++ 260417/일 수 구하기/number-of-days-in-month.cpp
@@ -6,15 +6,17 @@ int main() {
     int n;
     cin>>n;
 
-    if(n%4!=0)//윤년이 아닌 해
+    if(n!=0)
     {
-        if(n!=2)//홀수 해
+        if(n%2==0)//짝수 해
         {
-            cout<<31;
+            cout<<30;
         }
         else
-        cout<<28;
+        cout<<31;
         
     }
+    else
+    cout<<28;
     return 0;
 }
