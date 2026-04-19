b83302f4bd221ffa24ed2eb4abb2fe4f8506afb2
yjs0601 <soo00@tukorea.ac.kr>
2026-04-19 13:27:48 +0900

    Title: 중앙값 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/중앙값 구하기/find-the-median.cpp
+++ 260419/중앙값 구하기/find-the-median.cpp
@@ -5,19 +5,22 @@ int main() {
     // Please write your code here.
     int A,B,C;
     int mid=0;
-    cin >>A>>B>>C;
-    if(A>B&&A<C)
-    {
-        mid=A;
-    }
-    else if(B>A&&B<C)
-    {
-        mid=B;
-    }
-    else
-    mid=C;
 
-    cout<<mid;
+    cin >>A>>B>>C;
+
+   if((A>B)&&(A<C)||(A<B)&&(A>C))
+   {
+    mid=A;
+   }
+   else if((B>A)&&(B<C)||(B<A)&&(B>C))
+   {
+    mid=B;
+   }
+   else
+   mid=C;
+   
+   cout<<mid;
 
     return 0;
 }
+
