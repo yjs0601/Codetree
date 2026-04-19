af35e9a7f5b659b9f85e35b2354e2269991d9956
yjs0601 <soo00@tukorea.ac.kr>
2026-04-19 12:46:36 +0900

    Title: 코로나 메뉴얼, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260419/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int p1_tem;
+    char p1;
+
+    int A=0;
+    int B=0;
+    int C=0;
+    int D=0;
+
+    cin>>p1>>p1_tem;
+   // cin>>p2>>p2_tem;
+    //cin>>p3>>p3_tem;
+
+    for(int i=1;i<=3;i++)
+    {
+        cin>>p1>>p1_tem;
+        if(p1=='Y')
+        {
+
+            if(p1_tem>=37)
+            {
+                
+                A++;
+            }
+            else
+                C++; //증상은 있지만 체온이 정상
+        }
+        else
+        if(p1_tem>=37)//증상은 없는데 열이 37도이상
+        {
+            B++;
+        }
+        else 
+        D++; // 증상도 없고 열도 없고
+
+        
+    }
+    if(A>=2){
+        cout<<'E';
+    }
+    else
+    cout<<"N";
+
+   
+    return 0;
+}
