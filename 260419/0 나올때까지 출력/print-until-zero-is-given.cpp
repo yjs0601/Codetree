0438ab801948ec523977d3630b85b25fa3387c67
yjs0601 <soo00@tukorea.ac.kr>
2026-04-19 15:42:07 +0900

    Title: 0 나올때까지 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260419/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    
+    while(true)
+    {
+        cin>>n;
+
+        if(n!=0)
+        {
+            cout<<n<<endl;
+        }
+        else
+        break;
+    }
+    
+    return 0;
+}
