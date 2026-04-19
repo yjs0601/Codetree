f311f5649b10efe7d0eed4c37a8d5894900badb8
yjs0601 <soo00@tukorea.ac.kr>
2026-04-19 15:44:45 +0900

    Title: 특정 수 맞히기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260419/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    while(true)
+    {
+        cin>>n;
+
+        if(n<25)
+        {
+            cout<<"Higher"<<endl;
+        }
+        else if(n>25)
+        {
+            cout<<"Lower"<<endl;
+        }
+        else
+        {
+            cout<<"Good"<<endl;
+            break;
+        }
+        
+    }
+    return 0;
+}
